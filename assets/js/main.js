39a167a1fb58412f1dbae159a3e030e154b58c06
hani sleman <hani.sulaiman.2002@gmail.com>
2024-05-20 15:56:51 +0300

    create project

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const navbar = document.querySelector('header');
+//     const changePoint = 50; // Pixels from top where the background color changes
+
+//     window.addEventListener('scroll', () => {
+//         if (window.scrollY > changePoint) {
+//             navbar.style.backgroundColor = 'var(--light-secondary)';
+//         } else {
+//             navbar.style.backgroundColor = 'transparent';
+//         }
+//     });
+// });
