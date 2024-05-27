789c78d45a3c19df6b298179fdefc16eec8cc801
hani sleman <hani.sulaiman.2002@gmail.com>
2024-05-27 14:48:39 +0300

    new update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const navbar = document.querySelector('header');
-//     const changePoint = 50; // Pixels from top where the background color changes
+document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.querySelector('header');
+    const changePoint = 50; // Pixels from top where the background color changes
 
-//     window.addEventListener('scroll', () => {
-//         if (window.scrollY > changePoint) {
-//             navbar.style.backgroundColor = 'var(--light-secondary)';
-//         } else {
-//             navbar.style.backgroundColor = 'transparent';
-//         }
-//     });
-// });
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > changePoint) {
+            // navbar.style.backgroundColor = 'var(--light-secondary)';
+            navbar.classList.add('scroll-active');
+        } else {
+            navbar.classList.remove('scroll-active');
+        }
+    });
+});
