b40e9e6676abf79f8c6323b0d07b70e81b205ffd
hani sleman <hani.sulaiman.2002@gmail.com>
2024-05-27 17:40:11 +0300

    new update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navbar = document.querySelector('header');
-    const changePoint = 50; // Pixels from top where the background color changes
 
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > changePoint) {
-            // navbar.style.backgroundColor = 'var(--light-secondary)';
-            navbar.classList.add('scroll-active');
-        } else {
-            navbar.classList.remove('scroll-active');
-        }
-    });
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.querySelector('body.home header')) {
+        const navbar = document.querySelector('body.home header');
+
+        const changePoint = 50; // Pixels from top where the background color changes
+
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > changePoint) {
+                // navbar.style.backgroundColor = 'var(--light-secondary)';
+                navbar.classList.add('scroll-active');
+            } else {
+                navbar.classList.remove('scroll-active');
+            }
+        });
+    }
 });
+// document.addEventListener('DOMContentLoaded', () => {
+//     if (document.querySelector('.product-details')) {
+//         const productDetails = document.querySelector('body.home header');
+
+//         const changePoint = 400; // Pixels from top where the background color changes
+
+//         window.addEventListener('scroll', () => {
+//             if (window.scrollY > changePoint) {
+
+//                 productDetails.classList.add('scroll-active');
+//             } else {
+//                 productDetails.classList.remove('scroll-active');
+//             }
+//         });
+//     }
+// });
+
+
+
+let paragraphs = document.querySelectorAll('.title-background p');
+let counter = paragraphs.length;
+for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style = `animation-delay:${counter / 20}s`;
+    counter--;
+}
+//
